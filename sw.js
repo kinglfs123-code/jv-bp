d24f0da877747f5243f29c676b155ad5eb4b344b
kinglfs123-code <kinglfs123@outlook.com>
2026-09-17 09:11:47 -0300

    Update cache version from v8 to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bp-financeiro-v8';
+const CACHE = 'bp-financeiro-v9';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png',
                 './icon-maskable.png','./favicon.png'];
 
