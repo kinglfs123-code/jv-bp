1de501501463989f9a99f84a43ee651904ba9913
kinglfs123-code <kinglfs123@outlook.com>
2026-09-25 10:26:25 -0300

    Update cache version from v25 to v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bp-financeiro-v25';
+const CACHE = 'bp-financeiro-v26';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png',
                 './icon-maskable.png','./favicon.png'];
 
