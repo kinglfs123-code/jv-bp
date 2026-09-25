3a9b62b8d3c2d7bf2d39db7e079874721ac1a755
kinglfs123-code <kinglfs123@outlook.com>
2026-09-25 08:40:53 -0300

    Update cache version from v20 to v22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bp-financeiro-v20';
+const CACHE = 'bp-financeiro-v22';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png',
                 './icon-maskable.png','./favicon.png'];
 
